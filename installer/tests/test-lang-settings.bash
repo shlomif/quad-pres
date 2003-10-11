ea8c59543a8e6a1e0b13e381bf360917f20e5ee1
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2003-10-11 20:20:17 +0000

    Fixed and completing the set-lang test.

--- installer/tests/test-lang-settings.bash
+++ installer/tests/test-lang-settings.bash
@@ -7,9 +7,24 @@ else
 fi
 cd "in-out"
 
-# Check that the default 
-t=1
-test_dir=testlang$t 
+# Check that the default charset and lang are OK.
+t=0
+
+perform_test()
+{
+let t++
+echo "Test No. $t"
+global_to_set_lang="$1"
+shift
+global_to_set_charset="$1"
+shift
+local_to_set_lang="$1"
+shift
+local_to_set_charset="$1"
+shift
+echo "Args: \"$global_to_set_lang\" \"$global_to_set_charset\" \"$local_to_set_lang\" \"$local_to_set_charset\""
+    
+test_dir=testlang$t
 quadp setup $test_dir --dest-dir=`pwd`/${test_dir}-output
 cat > $test_dir/src/index.html.wml <<EOF
 #include 'template.wml'
@@ -19,19 +34,30 @@ Hello world!
 </p>
 
 EOF
+
+wanted="$(perl ../set-lang-settings.pl "$test_dir" "$global_to_set_lang" "$global_to_set_charset" "$local_to_set_lang" "$local_to_set_charset")"
+
+echo "Got \"$wanted\" as a result"
+
 (cd $test_dir && quadp render -a)
 output_file=$test_dir-output/index.html
 if ! tidy -errors $output_file ; then
-    echo "File does not validate!" 1>2 ; 
+    echo "File does not validate!" 1>&2 
     exit 
 fi
-if ! perl ../verify-lang-settings.pl iso-8859-1 en-US < $output_file ; then
-    echo "File has improper language encodings" 1>2 
+if ! perl ../verify-lang-settings.pl $wanted < $output_file ; then
+    echo "File has improper language encodings" 1>&2 
     exit
 fi
+}
 
-# TODO:
-# Write a test setting the encoding and/or language to something else
-
-exit 1
+for lang1 in "" "he-IL" ; do
+    for char1 in "" "utf-8" ; do
+        for lang2 in "" "en-GB" ; do
+            for char2 in "" "iso-8859-8" ; do
+                perform_test "$lang1" "$char1" "$lang2" "$char2"
+            done
+        done
+    done
+done
 
