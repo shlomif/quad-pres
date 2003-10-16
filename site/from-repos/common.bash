fb6983e6f20c8942ca7e43c970329c03f1ab86e7
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2003-10-16 11:34:24 +0000

    Changed to version 0.10.1.

--- site/from-repos/common.bash
+++ site/from-repos/common.bash
@@ -1,6 +1,7 @@
-root="http://localhost:8080/svn/repos/progs/quad-pres/"
+root="http://stalker.iguide.co.il:8080/svn/lm-solve/quad-pres/"
 devel_dir="devel"
 prog_name="quad-pres"
+stable_dir="stable"
 
 mkdir_if_not_exists()
 {
