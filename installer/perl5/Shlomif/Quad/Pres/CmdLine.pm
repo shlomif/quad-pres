ae6909fd59dd481f2598b9d7a484537f27a9a839
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2006-08-31 19:46:29 +0000

    Converted Render_all_contents into a method instead of a script.

--- installer/perl5/Shlomif/Quad/Pres/CmdLine.pm
+++ installer/perl5/Shlomif/Quad/Pres/CmdLine.pm
@@ -2,6 +2,8 @@ package Shlomif::Quad::Pres::CmdLine;
 
 use strict;
 
+use Scalar::Util qw(blessed);
+
 use Shlomif::Gamla::Object;
 
 use vars qw(@ISA);
@@ -18,6 +20,9 @@ use File::Copy;
 use Shlomif::Quad::Pres::Path;
 use Shlomif::Quad::Pres::Exception;
 use Shlomif::Quad::Pres::Getopt;
+use Shlomif::Quad::Pres::Config;
+use Shlomif::Quad::Pres;
+use Shlomif::Quad::Pres::FS;
 
 my $error_class = "Shlomif::Quad::Pres::Exception";
 
@@ -391,19 +396,184 @@ sub perform_render_command
 
     $self->chdir_to_base();
 
-    my $scripts_dir = $self->{'path_man'}->get_scripts_dir();
-    $self->run_command(
-        'command' => "$scripts_dir/Render_all_contents.pl",
-        'error_text' => "Rendering Failed!",
-    );
+    eval {
+    $self->_render_all_contents();
+    };
+    my $error = $@;
+    if (defined($error) && blessed($error) && $error->isa('Shlomif::Quad::Pres::Exception::RenderFile'))
+    {
+        throw $error_class -text => "Rendering Failed!";
+    }
+    
+    # $self->run_command(
+    #     'command' => "$scripts_dir/Render_all_contents.pl",
+    #     'error_text' => "Rendering Failed!",
+    # );
 
     if ($render_hard_disk_html)
     {
+        my $scripts_dir = $self->{'path_man'}->get_scripts_dir();
         $self->run_command(
             'command' => "$scripts_dir/html-server-to-hd.pl",
             'error_text' => "Conversion to Hard-disk format failed",
         );
     }
+}
+
+sub _render_all_contents
+{
+    my $self = shift;
+
+    my $cfg = Shlomif::Quad::Pres::Config->new();
+
+    my $path_man = Shlomif::Quad::Pres::Path->new();
+    my $scripts_dir = $path_man->get_scripts_dir();
+
+    my $default_dest_dir = $cfg->get_server_dest_dir();
+    my $render_type = "server";
+
+    require Contents;
+    my $contents = Contents::get_contents();
+
+    my $src_dir = "./src/";
+    my $dest_dir = shift || $default_dest_dir;
+
+    if ($src_dir !~ /\/$/)
+    {
+        $src_dir .= "/";
+    }
+    if ($dest_dir !~ /\/$/)
+    {
+        $dest_dir .= "/";
+    }
+
+    my $group = $cfg->get_setgid_group();
+
+    my $fs_iface = Shlomif::Quad::Pres::FS->new('group' => $group);
+
+    # Check if the destination directory exists and if not -
+    # create it.
+    $fs_iface->make_dest_dir($dest_dir);
+
+    my $render_all = 0;
+
+    my $get_file_mtime = sub {
+        my $path = shift;
+
+        return (stat($path))[9];
+    };
+
+    my (@main_files_mtimes) = map { $get_file_mtime->($_) } ("Contents.pm", "template.wml");
+
+    my $traverse_callback = sub {
+        my (%arguments) = (@_);
+        
+        my $path_ref = $arguments{'path'};
+        my $branch = $arguments{'branch'};
+
+        my (@path);
+
+        @path = @{$path_ref};
+
+        my $p = join("/", @path);
+
+        my ($filename, $src_filename);
+
+        my $is_dir = exists($branch->{'subs'});
+
+        if ($is_dir)
+        {
+            # It is a directory
+            $filename = ($dest_dir . "/" . $p);
+            if (! (-d $filename))
+            {
+                mkdir($filename);
+            }
+            $filename .= "/index.html";
+            $src_filename = $src_dir . "/" . $p . "/index.html";
+        }
+        else
+        {
+            $filename = ($dest_dir . "/" . $p);
+            $src_filename = $src_dir . "/" . $p;
+        }
+        $src_filename .= ".wml";
+        # Automatically copy the template to the source filename
+        if (! -e $src_filename)
+        {
+            if ($is_dir)
+            {
+                my $dir_name = $src_filename;
+                $dir_name =~ s/\/*index\.html\.wml$//;
+                mkdir($dir_name);
+            }
+            
+            open I, "<template.html.wml";
+            open O, (">" . $src_filename);
+            binmode(I);
+            binmode(O);
+            print O join("", <I>);
+            close(O);
+            close(I);
+        }
+        
+        my $src_mtime = $get_file_mtime->($src_filename);
+        my $dest_mtime = $get_file_mtime->($filename);
+
+        if ((! -e $filename) || 
+            (grep 
+                { $_ > $dest_mtime } 
+                (@main_files_mtimes,$src_mtime)
+            ))
+        {
+            my $src_filename_modified = $src_filename;
+            $src_filename_modified =~ s/^(\.\/)?src\/*//;
+            my $cmd = "$scripts_dir/render-file.pl \"$src_filename_modified\" > \"$filename\"\n";
+                
+            print $cmd, "\n";
+            if (system($cmd) != 0) 
+            {
+                # Clean-up the file so it will have to be regenerated
+                unlink($filename);
+                my $error = Shlomif::Quad::Pres::Exception::RenderFile->new();
+                $error->{'src_filename'} = $src_filename;
+                die $error;
+            }
+        }
+
+        if (exists($branch->{'images'}))
+        {
+            foreach my $image (@{$branch->{'images'}})
+            {
+                $filename = $dest_dir . "/" . $p . "/" . $image ;
+                $src_filename = $src_dir . "/" . $p . "/" . $image ;
+
+                my $src_mtime = $get_file_mtime->($src_filename);
+                my $dest_mtime = $get_file_mtime->($filename);
+                if ((! -e $filename) ||
+                    ($src_mtime > $dest_mtime)
+                    )
+                {
+                    open I, ( "<" . $src_filename);
+                    open O, ( ">" . $filename);
+                    binmode(I);
+                    binmode(O);
+                    print O join("",<I>);
+                    close(O);
+                    close(I);        
+                }
+            }
+        }
+    };
+
+    my $quadpres_obj = 
+        Shlomif::Quad::Pres->new(
+            $contents, 
+            'doc_id' => "/",
+            'mode' => "server",
+        );
+
+    $quadpres_obj->traverse_tree($traverse_callback);
 }
 
 sub perform_clear_command
