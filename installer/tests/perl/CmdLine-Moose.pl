94afeec28fe7e15a6d05b41d903262996153e6e3
Shlomi Fish <shlomif@shlomifish.org>
2006-09-05 13:08:34 +0000

    More refactoring of Shlomif::Quad::Pres::CmdLine.

--- installer/tests/perl/CmdLine-Moose.pl
+++ installer/tests/perl/CmdLine-Moose.pl
@@ -5,7 +5,7 @@ use warnings;
 
 use Shlomif::Quad::Pres::CmdLine;
 
-my $o = Shlomif::Quad::Pres::CmdLine->new();
+my $o = Shlomif::Quad::Pres::CmdLine->new('cmd_line' => ["render", "-a"]);
 
 $o->src_dir("Hello");
 if ($o->src_dir() ne "Hello")
