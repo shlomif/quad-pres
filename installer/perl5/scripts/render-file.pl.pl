001aa3e9f1061ccfb9dd05b67e3d65c0f5251ba0
Shlomi Fish <shlomif@shlomifish.org>
2007-05-19 09:03:27 +0000

    1. Eliminated the use of the upload script.
    2. Added the QUAD_PRES_NO_HOME_LIB environment variable option to
    eliminate the use of the -I ~/.Quad-Pres/lib. This is so the tests can
    run correctly.
    
    3. Moved the <qp:before_*> macros to the quadpres_main.wml file.
    
    4. Added the QUAD_PRES_NO_HOME_LIB setting to the Makefile.am under purecheck
    so the file will work correctly.

--- installer/perl5/scripts/render-file.pl.pl
+++ installer/perl5/scripts/render-file.pl.pl
@@ -16,10 +16,16 @@ my $modules_dir = $path_man->get_modules_dir();
 
 my $local_wml_dir = $ENV{"HOME"}. "/.Quad-Pres/lib/";
 
+my @local_wml = 
+    $ENV{"QUAD_PRES_NO_HOME_LIB"} 
+        ? ()
+        : ("-I", $local_wml_dir)
+    ;
+
 exec (
     "wml",
     "-I", $wml_dir,
-    "-I", $local_wml_dir,
+    @local_wml,
     "-DFILENAME=$filename",
     "--passoption=3,-I$modules_dir",
     "src/$filename.wml"
