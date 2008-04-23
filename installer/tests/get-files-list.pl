b194c96c93375def83f0fd3f59976da0ab97be22
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2008-04-23 10:41:57 +0000

    Adapted get-files-list.pl to the new svn semantics - this time using an svn command.

--- installer/tests/get-files-list.pl
+++ installer/tests/get-files-list.pl
@@ -1,70 +1,25 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 use strict;
+use warnings;
 
 use File::Copy;
-
-use XML::LibXML;
+use String::ShellQuote;
 
 my $files_or_dirs = shift || "-f";
 
 $files_or_dirs = ($files_or_dirs eq "-f");
 
-my $source_dir = shift || ".";
+my $source_dir = shell_quote(shift || ".");
+
+my @lines = `svn ls -R $source_dir`;
 
 my (@dirs, @files);
-sub process_dir
-{
-    my $dir = shift;
 
-    my $dir_slash = ($dir eq "") ? "" : "$dir/";
-
-    if ($dir ne "")
-    {
-        push @dirs, $dir;
-    }
-
-    my (@entries);
-    
-    {
-        my $parser = XML::LibXML->new();
-
-        my $dom = $parser->parse_file("$source_dir/${dir_slash}.svn/entries");
-
-        my @svn_entries = $dom->getDocumentElement()->getChildrenByTagName("entry");
-
-        foreach (@svn_entries)
-        {
-            my $deleted = $_->getAttribute("deleted");
-            if (defined($deleted) && ($deleted eq "true"))
-            {
-                next;
-            }
-            if ($_->hasAttribute("url"))
-            {
-                next;
-            }
-
-            push @entries, $_->getAttribute("name");
-        }
-    }
-
-    foreach my $entry (@entries)
-    {
-        my $rel_path = "${dir_slash}$entry";
-        my $full_path = "$source_dir/$rel_path";
-        if (-d $full_path)
-        {
-            process_dir($rel_path);
-        }
-        else
-        {
-            push @files, $rel_path;
-        }
-    }    
-}
-
-process_dir("");
-
-print map { "$_\n" } (($files_or_dirs) ? (@files) : (@dirs));
-
+print
+    grep
+    { my $f = $_; chomp($f); $files_or_dirs ? (-f $f) : (-d $f); }
+    sort
+    { $a cmp $b }
+    @lines
+    ;
