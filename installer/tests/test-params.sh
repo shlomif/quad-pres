e98f24d01ef651fcb134969b9ea6d790313f053b
Shlomi Fish <shlomif@shlomifish.org>
2009-08-09 13:16:05 +0000

    Now handling the PERL5LIB better.

--- installer/tests/test-params.sh
+++ installer/tests/test-params.sh
@@ -1,2 +1,2 @@
-PATH=`pwd`/installation/bin:$PATH
-export PERL5LIB=`pwd`/installation/share/quad-pres/perl5/
+PATH="`pwd`/installation/bin:$PATH"
+export PERL5LIB="`pwd`/installation/share/quad-pres/perl5/:$PERL5LIB"
