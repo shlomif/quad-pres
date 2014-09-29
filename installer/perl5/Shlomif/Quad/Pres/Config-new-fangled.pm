35d81f2ee09d94c6542e4b5b8916124788c46bc0
Shlomi Fish <shlomif@shlomifish.org>
2014-09-29 13:29:02 +0300

    refactoring.

--- installer/perl5/Shlomif/Quad/Pres/Config-new-fangled.pm
+++ installer/perl5/Shlomif/Quad/Pres/Config-new-fangled.pm
@@ -1,29 +1,22 @@
 package Shlomif::Quad::Pres::Config;
 
 use strict;
+use warnings;
 
-use Shlomif::Gamla::Object;
+use parent 'Games::Solitaire::Verify::Base';
 
-use vars qw(@ISA);
-
-@ISA=qw(Shlomif::Gamla::Object);
+__PACKAGE__->mk_acc_ref([qw( base_path cfg )]);
 
 use Config::IniFiles;
 
 sub initialize
 {
     my $self = shift;
+    my %args = @_;
 
-    my $base_path = ".";
+    my $base_path = $args{path} || ".";
 
-    $self->initialize_analyze_args(
-        {
-            '[Pp]ath' => sub { my $new_path = shift; $base_path = $new_path; }
-        },
-        @_
-    );
-
-    $self->{'base_path'} = $base_path;
+    $self->base_path($base_path);
 
     my $cfg =
         Config::IniFiles->new(
@@ -35,7 +28,7 @@ sub initialize
         die "Could not open the configuration file!";
     }
 
-    $self->{'cfg'} = $cfg;
+    $self->cfg( $cfg );
 
     return 0;
 }
@@ -44,28 +37,28 @@ sub get_server_dest_dir
 {
     my $self = shift;
 
-    return $self->{'cfg'}->val("quadpres", "server_dest_dir");
+    return $self->cfg->val("quadpres", "server_dest_dir");
 }
 
 sub get_setgid_group
 {
     my $self = shift;
 
-    return $self->{'cfg'}->val("quadpres", "setgid_group");
+    return $self->cfg->val("quadpres", "setgid_group");
 }
 
 sub get_rsync_upload_path
 {
     my $self = shift;
 
-    return $self->{'cfg'}->val("rsync", "upload_path");
+    return $self->cfg->val("rsync", "upload_path");
 }
 
 sub get_version_control
 {
     my $self = shift;
 
-    return $self->{'cfg'}->val("quadpres", "version_control");
+    return $self->cfg->val("quadpres", "version_control");
 }
 
 1;
