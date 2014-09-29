697f8b265cb533188fbfff58fe2de80cac36a51b
Shlomi Fish <shlomif@shlomifish.org>
2014-09-29 11:22:38 +0300

    more.

---
+++ installer/run_test.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Cwd ();
+use IO::All qw / io /;
+
+use File::Path qw / rmtree /;
+my $myprefix = Cwd::getcwd() . "/tests/installation";
+
+my $build_dir = "FOO";
+
+rmtree ([$build_dir]);
+if (system("mkdir $build_dir && cd $build_dir && cmake -DCMAKE_INSTALL_PREFIX=$myprefix ../../installer && make && make install"))
+{
+    die "cmake Failed";
+}
+
+$ENV{PERL5LIB} = "$myprefix/sharequad-pres/perl5:$ENV{PERL5LIB}";
+$ENV{QUAD_PRES_NO_HOME_LIB} = 1;
+$ENV{PATH} = "$myprefix/bin:$ENV{PATH}}";
+
+exec("cd tests && make check");
