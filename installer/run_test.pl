8729727ad6ee3f885c5be239162c6d6c794eed27
Shlomi Fish <shlomif@shlomifish.org>
2014-10-10 18:46:34 +0300

    Fix the tests with the new CMake installation.

--- installer/run_test.pl
+++ installer/run_test.pl
@@ -17,7 +17,7 @@ if (system("mkdir $build_dir && cd $build_dir && cmake -DCMAKE_INSTALL_PREFIX=$m
     die "cmake Failed";
 }
 
-$ENV{PERL5LIB} = "$myprefix/lib/perl5:$ENV{PERL5LIB}";
+$ENV{PERL5LIB} = "$myprefix/share/quad-pres/perl5:$ENV{PERL5LIB}";
 $ENV{QUAD_PRES_NO_HOME_LIB} = 1;
 $ENV{PATH} = "$myprefix/bin:$ENV{PATH}";
 
