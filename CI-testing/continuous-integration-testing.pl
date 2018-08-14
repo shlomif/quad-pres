415888ea0794c552cfcfb6a77d2829bee5fd9733
Shlomi Fish <shlomif@shlomifish.org>
2018-08-14 05:21:03 +0300

    Add a .travis.yml file for Travis-CI.
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

---
+++ CI-testing/continuous-integration-testing.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+sub do_system
+{
+    my ($args) = @_;
+
+    my $cmd = $args->{cmd};
+    print "Running [@$cmd]";
+    if ( system(@$cmd) )
+    {
+        die "Running [@$cmd] failed!";
+    }
+}
+
+my $IS_WIN = ( $^O eq "MSWin32" );
+my $SEP    = $IS_WIN ? "\\" : '/';
+my $MAKE   = $IS_WIN ? 'gmake' : 'make';
+
+my $cmake_gen;
+if ($IS_WIN)
+{
+    $cmake_gen = 'MSYS Makefiles';
+}
+my $cmd = shift @ARGV;
+
+# do_system({cmd => ["cd black-hole-solitaire/ && mkdir B && cd B && ../c-solver/Tatzer && make && $^X ../c-solver/run-tests.pl"]});
+
+# do_system({cmd => ["cd black-hole-solitaire/Games-Solitaire-BlackHole-Solver/ && dzil test --all"]});
+
+my @dzil_dirs = ();
+
+my $CPAN = sprintf( '%scpanm', ( $IS_WIN ? '' : 'sudo ' ) );
+if ( $cmd eq 'install_deps' )
+{
+    foreach my $d (@dzil_dirs)
+    {
+        do_system(
+            {
+                cmd => [
+"cd $d && (dzil authordeps --missing | $CPAN) && (dzil listdeps --author --missing | $CPAN)"
+                ]
+            }
+        );
+    }
+}
+elsif ( $cmd eq 'test' )
+{
+    foreach my $d (@dzil_dirs)
+    {
+        do_system( { cmd => ["cd $d && (dzil smoke --release --author)"] } );
+    }
+    do_system(
+        {
+            cmd => [
+                      "cd installer/ && mkdir B && cd B && $^X ..${SEP}Tatzer "
+                    . ( defined($cmake_gen) ? qq#--gen="$cmake_gen"# : "" )
+                    . " .. && $MAKE"
+            ]
+        }
+    );
+}
+else
+{
+    die "Unknown command '$cmd'!";
+}
