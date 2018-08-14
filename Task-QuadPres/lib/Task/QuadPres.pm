d80d264492452dc0619d620389cba107a6dd5478
Shlomi Fish <shlomif@shlomifish.org>
2018-08-14 07:57:25 +0300

    Add the task.

---
+++ Task-QuadPres/lib/Task/QuadPres.pm
@@ -0,0 +1,54 @@
+package Task::QuadPres;
+
+use strict;
+use warnings;
+
+1;
+
+=head1 NAME
+
+Task::QuadPres - install the CPAN dependencies of Quad-Pres.
+
+=head1 DESCRIPTION
+
+This task installs modules needed by Quad-Pres:
+
+L<http://www.shlomifish.org/quad-pres/>
+
+=pkgroup QuadPres deps
+
+=pkg CGI
+
+=pkg Class::XSAccessor
+
+=pkg Config::IniFiles
+
+=pkg Data::Dumper
+
+=pkg File::Copy::Recursive
+
+=pkg File::Find
+
+=pkg File::Find::Object::Rule
+
+=pkg Getopt::Long
+
+=pkg HTML::Links::Localize
+
+=pkg HTML::Lint
+
+=pkg HTML::Widgets::NavMenu::EscapeHtml
+
+=pkg IO::All
+
+=pkg MIME::Types
+
+=pkg MooX
+
+=pkg MooX::late
+
+=pkg Path::Tiny
+
+=pkg Template
+
+=pkg Test::Trap
