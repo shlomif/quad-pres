2262f09ea47738c637663391bb03ed06a3e6af00
Shlomi Fish <shlomif@shlomifish.org>
2019-04-14 00:29:17 +0300

    tidyall / dzil cleanups

--- installer/perl5/Shlomif/Quad/Pres/Config-new-fangled.pm
+++ installer/perl5/Shlomif/Quad/Pres/Config-new-fangled.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use parent 'Shlomif::Quad::Pres::Base';
 
-__PACKAGE__->mk_acc_ref([qw( base_path cfg )]);
+__PACKAGE__->mk_acc_ref( [qw( base_path cfg )] );
 
 use Config::IniFiles;
 
@@ -18,17 +18,14 @@ sub initialize
 
     $self->base_path($base_path);
 
-    my $cfg =
-        Config::IniFiles->new(
-            -file => "$base_path/quadpres.ini"
-        );
+    my $cfg = Config::IniFiles->new( -file => "$base_path/quadpres.ini" );
 
-    if (!defined($cfg))
+    if ( !defined($cfg) )
     {
         die "Could not open the configuration file!";
     }
 
-    $self->cfg( $cfg );
+    $self->cfg($cfg);
 
     return 0;
 }
@@ -37,30 +34,29 @@ sub get_server_dest_dir
 {
     my $self = shift;
 
-    return $self->cfg->val("quadpres", "server_dest_dir");
+    return $self->cfg->val( "quadpres", "server_dest_dir" );
 }
 
 sub get_setgid_group
 {
     my $self = shift;
 
-    return $self->cfg->val("quadpres", "setgid_group");
+    return $self->cfg->val( "quadpres", "setgid_group" );
 }
 
 sub get_rsync_upload_path
 {
     my $self = shift;
 
-    return $self->cfg->val("rsync", "upload_path");
+    return $self->cfg->val( "rsync", "upload_path" );
 }
 
 sub get_version_control
 {
     my $self = shift;
 
-    return $self->cfg->val("quadpres", "version_control");
+    return $self->cfg->val( "quadpres", "version_control" );
 }
 
 1;
-
 
